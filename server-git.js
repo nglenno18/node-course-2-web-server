459ec9fc1a177556c6fa0d3f53ec0d7caac2e36b
nglenno18 <nglenno18@gmail.com>
2016-11-28 12:18:34 -0600

    Setup start script and Heroku port

--- server-git.js
+++ server-git.js
@@ -3,11 +3,19 @@ Section 5, Lecture 46 -- ADDING VERSION CONTROL (GIT)
   Use Git to save project,
   back up work to GitHub,
   and deploy project LIVE to web (not just available on localhost)
+
+Section 5, Lecture 47 -- GITHUB and SSHKeys
+  -use GitBash, not the command prompt
+Section 5, Lecture 48 -- DEPLOYING APPS
+  - ssh, gitbash, heroku
 */
 
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+//if process.env.port (local) does not exits, use a default 3000
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -37,7 +45,6 @@ app.use(function(req, resp, next){    //next exists so you can tell your middlew
   next();  //app will only continue when next() is called
 });
 
-
 app.use('/maintenance', function(req, resp, next){
   resp.render('maintenance.hbs', {
     pageTitle: 'Maintenance HBS',
@@ -52,7 +59,6 @@ app.use('/maintenance', function(req, resp, next){
 //^^middleware is executed in the order it is used
 //help.html will still render. We set up our express.static directory, then our logger, then maintenance hbs logger
 
-
 app.get('/', function(request, response){         //request = http request
   response.render('home.hbs', {
     pageTitle: 'Home Page',
@@ -60,9 +66,7 @@ app.get('/', function(request, response){         //request = http request
   });
 });
 
-//handler takes the response AND request object
 app.get('/about', function(req, res){
-  //res.send('About Page');
   res.render('about.hbs', {
     pageTitle: 'About Page'
   });
@@ -72,18 +76,22 @@ app.get('/about', function(req, res){
 CHALLENGE: Create a route to /bad --> send back json with an errorMessage
 */
 app.get('/bad', function(req, res){
-  // res.send('Bad Page');   --Instead of passing in a string or html, pass in object
   res.send({
     errorMessage: 'Unable to handle request'
   });
 });
-//app.listen is going to bind application to a port on our machine
-app.listen(3000, function(){
-  console.log('Server is up and running on Port: 3000');
+
+//The port will not be static, it will be an environment variable that heroku will set
+//    -heroku tells app which port to use, which changes
+//app.listen(3000, function(){
+app.listen(port, function(){
+  console.log(`Server is up and running on Port: ${port}`);
 });
 console.log('check the client');
 
-/*
-Very basic express application that listens on port 3000 and
-  currently has handlers for 3 URLs
-*/
+//Next: Specify a script in package.json (add 'node server-git.js' cmd to the test script)
+// "start": "node server.js"
+// when heroku tries to start app, will not run node w/ file name, because doesnt know what it is
+    // so it runs the start script from package.json
+    //--> we run app using start script from terminal
+    //-- npm start
